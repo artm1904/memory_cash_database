000ea19685af216f5299a48cea16a64cad72f5b9
artm1904 <artmsavchenko@yandex.ru>
2025-07-28 21:44:14 +0300

    Made a init_server(), accept_connection() and child_loop() func

--- my_database/include/server.hpp
+++ my_database/include/server.hpp
@@ -1,11 +1,26 @@
 #pragma once
 
-#include <iostream>
-
+#include <arpa/inet.h>  // для inet_addr()
 #include <netinet/in.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>  // для inet_addr()
 #include <unistd.h>  // для close()
 
-#define PORT 12000
+#include <iostream>
+#include <memory>
+#include <string>
+
+#define PORT 12002
 #define HOST "127.0.0.1"
+
+struct s_client {
+    int client_fd;
+
+    std::string ip;
+    int port;
+};
+
+using Client = struct s_client;
+
+int init_server();
+void accept_connection(int sock_fd);
+void child_loop(std::shared_ptr<Client> client);
--- my_database/source/server.cpp
+++ my_database/source/server.cpp
@@ -1,8 +1,9 @@
 #include "server.hpp"
 
 bool server_continue;
+bool child_continue;
 
-int server_loop() {
+int init_server() {
     struct sockaddr_in sock;
     int sock_fd;
 
@@ -16,7 +17,7 @@ int server_loop() {
 
     sock_fd = socket(AF_INET, SOCK_STREAM, 0);  // SOCK_STREAM - use TCP
 
-    if (sock_fd > 0) {
+    if (sock_fd < 0) {
         std::cerr << "Socket doesn't created" << std::endl;
         return -1;
     }
@@ -30,12 +31,72 @@ int server_loop() {
         std::cerr << "Failed to prepare to accept connections on socket FD" << std::endl;
         return -1;
     }
+
+    std::cout << "Server started listening on " << HOST << ":" << PORT << std::endl;
+
+    return sock_fd;
+}
+
+void accept_connection(int sock_fd) {
+    struct sockaddr_in client;
+    int client_fd;
+
+    pid_t pid;
+
+    char ip[16];
+    int port;
+    int len = sizeof(client);
+
+    client_fd = accept(sock_fd, (struct sockaddr *)&client, (socklen_t *)&len);
+
+    if (client_fd < 0) {
+        return;
+    }
+
+    port = ntohs(client.sin_port);
+    inet_ntop(AF_INET, &client.sin_addr, ip, sizeof(ip));
+    auto new_client = std::make_shared<Client>();
+    new_client->client_fd = client_fd;
+    new_client->port = port;
+    new_client->ip = ip;
+
+    std::cout << "New connection from " << ip << ":" << port << std::endl;
+
+    pid = fork();
+    if (pid != 0) {
+        return;
+    } else {
+        child_continue = true;
+        dprintf(client_fd, "100 Connected to server\n");
+
+        while (child_continue) {
+            child_loop(new_client);
+        }
+
+        close(client_fd);
+        return;
+    }
+}
+
+void child_loop(std::shared_ptr<Client> client) {
+    sleep(1000);
+    child_continue = false;
+    return;
 }
 
 int main(int argc, char const *argv[]) {
+    int sock_fd;
     server_continue = true;
+    sock_fd = init_server();
+    if (sock_fd < 0) {
+        std::cerr << "Failed to init server" << std::endl;
+        return -1;
+    }
     while (server_continue) {
+        accept_connection(sock_fd);
     }
 
+    std::cout << "Server stopped" << std::endl;
+    close(sock_fd);
     return 0;
 }
